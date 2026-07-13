6334334d68183137a270ce3b0e5537dde61c08e5
Huynh Duc Dung <dunghd.it@gmail.com>
2026-07-14 07:04:41 +0800

    chore: sync version.ts to v0.8.0

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // This file is generated at build time by scripts/build.sh or CI
 // Do not edit manually - it will be overwritten on next build
-export const VERSION = "0.7.9";
+export const VERSION = "0.8.0";
